bed692e019b8e81fbcc2d41a8c895fd6b306e856
bmulhern2 <bmulhern09@gmail.com>
2020-12-05 12:57:57 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: t
 let router = require('./Routes/router')
 
 // Use Client Code
-app.use(express.static(path.join(__dirname, '../dist/buildABlog')))
-app.use('/', express.static(path.join(__dirname, '/dist/')))
+app.use(express.static(path.join(__dirname, '/dist/buildABlog')))
+app.use('/', express.static(path.join(__dirname, '/dist')))
 
 // Use Router
 app.use('/api', router)
